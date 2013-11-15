ddb4f338e394f8b226ec675f3b598731a369cf1b
heinetron <heisncfr@inf.upv.es>
2013-11-15 13:05:12 +0100

    Adds Offer and Demand methods
    Author: kerplo

--- database.php
+++ database.php
@@ -1,5 +1,35 @@
 <?php
 
+function clearTable($connection, $tableName){
+	
+	$sql="DELETE FROM ".$tableName;
+
+	// Execute query
+	if (mysqli_query($connection,$sql))
+	{
+		echo "Deleted all records from ".$tableName;
+	}
+	else
+	{
+		echo "Error deleting records: " . mysqli_error($connection);
+	}
+}
+
+
+function printTable($connection, $tableName)
+{
+	$result = mysqli_query($connection,"SELECT * FROM ".$tableName);
+	if(!$result){
+		echo "Error selecting from table ".$tableName.": " . mysqli_error($connection);
+	}
+	
+	while($row = mysqli_fetch_array($result))
+	{
+		var_dump($row);
+		echo "<br>";
+	}
+}
+
 function createDBConnection()
 {
 	// Create connection
@@ -12,10 +42,12 @@ function createDBConnection()
 	return $connection;
 }
 
+/////////////////////////////////////////////// USER ////////////////////////////////////////////////
+
 function createUserTable($connection)
 {
 	// Create table
-	$sql="CREATE TABLE IF NOT EXISTS Users(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), Name VARCHAR(40), Email VARCHAR(40), Password VARCHAR(120) NOT NULL)";
+	$sql="CREATE TABLE IF NOT EXISTS Users(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), Name VARCHAR(40) NOT NULL, Email VARCHAR(40) NOT NULL, Password VARCHAR(120) NOT NULL)";
 
 	// Execute query
 	if (mysqli_query($connection,$sql))
@@ -30,6 +62,7 @@ function createUserTable($connection)
 
 function insertUser($connection, $name, $email, $password)
 {
+	$password = getEncryptedPassword($connection, $password);
 	// Insert data
 	$result = mysqli_query($connection,"INSERT INTO `Users`(`Name`, `Email`, `Password`) VALUES ('".$name."', '".$email."', '".$password."')");
 	if(!$result){
@@ -37,23 +70,137 @@ function insertUser($connection, $name, $email, $password)
 	}
 }
 
-function printTable($connection, $tableName)
-{
-	$result = mysqli_query($connection,"SELECT * FROM ".$tableName);
-	if(!$result){
-		echo "Error selecting from table ".$tableName.": " . mysqli_error($connection);
-	}
-	while($row = mysqli_fetch_array($result))
+/////////////////////////////////////////////// SECURE ////////////////////////////////////////////////
+
+function createSaltTable($connection){
+	// Create table
+	$sql="CREATE TABLE IF NOT EXISTS Salt(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), Salt VARCHAR(32) NOT NULL)";
+	// Execute query
+	if (mysqli_query($connection,$sql))
 	{
-		echo $row['id'] . " " . $row['Name'] . " " . $row['Email'];
-		echo "<br>";
+		echo "Table created successfully";
+	}
+	else
+	{
+		echo "Error creating table: " . mysqli_error($connection);
 	}
 }
 
+function insertSalt($connection)
+{
+	//$salt = substr("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", mt_rand(0, 50) , 1) .substr( md5( time() ), 1);
+	$salt = md5( time() );
+	$result = mysqli_query($connection,"INSERT INTO `Salt`(`Salt`) VALUES ('".$salt."')");
+	if(!$result){
+		echo "Error inserting user: " . mysqli_error($connection);
+	}
+}
+
+function getSalt($connection, $saltId){
+	
+	$sql = "SELECT Salt FROM Salt WHERE id = ".$saltId;
+	$salt = mysqli_query($connection,$sql);
+	$salt = mysqli_fetch_array($salt);
+	return $salt[0];
+}
+
+function getEncryptedPassword($connection, $password){
+	
+	// Get the first salt
+	$salt = getSalt($connection, 1);
+	
+	//Encrypt password
+	$encryptedPassword = crypt("password", $salt);
+	
+	mysqli_close($con);
+	return $encryptedPassword;
+}
+
+/////////////////////////////////////////////// OFFERS ////////////////////////////////////////////////
+
+function createOfertTable($connection)
+{
+	// Create table
+	$sql="CREATE TABLE IF NOT EXISTS Ofert(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), Name VARCHAR(40), Category VARCHAR(40), Description VARCHAR(120) NOT NULL)";
+
+	// Execute query
+	if (mysqli_query($connection,$sql))
+	{
+		echo "Table created successfully";
+	}
+	else
+	{
+		echo "Error creating table: " . mysqli_error($connection);
+	}
+}
+
+function getOfert($connection){
+	
+	$sql = "SELECT * FROM Ofert";
+	$ofert = mysqli_query($connection,$sql);
+	$ofert = mysqli_fetch_array($ofert);
+	return $ofert;
+}
+
+function insertOfert($connection, $name, $category, $description)
+{
+	// Insert data
+	$result = mysqli_query($connection,"INSERT INTO `Ofert`(`Name`, `Category`, `Description`) VALUES ('".$name."', '".$category."', '".$description."')");
+	if(!$result){
+		echo "Error inserting ofert: " . mysqli_error($connection);
+	}
+}
+
+/////////////////////////////////////////////// DEMANDS ////////////////////////////////////////////////
+
+function createDemandTable($connection)
+{
+	// Create table
+	$sql="CREATE TABLE IF NOT EXISTS Demand(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), Name VARCHAR(40), Category VARCHAR(40), Description VARCHAR(120) NOT NULL)";
+
+	// Execute query
+	if (mysqli_query($connection,$sql))
+	{
+		echo "Table created successfully";
+	}
+	else
+	{
+		echo "Error creating table: " . mysqli_error($connection);
+	}
+}
+
+function getDemand($connection){
+	
+	$sql = "SELECT * FROM Ofert";
+	$demand = mysqli_query($connection,$sql);
+	$demand = mysqli_fetch_array($demand);
+	return $demand;
+}
+
+function insertDemand($connection, $name, $category, $description)
+{
+	// Insert data
+	$result = mysqli_query($connection,"INSERT INTO `Demand`(`Name`, `Category`, `Description`) VALUES ('".$name."', '".$category."', '".$description."')");
+	if(!$result){
+		echo "Error inserting ofert: " . mysqli_error($connection);
+	}
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 $connection = createDBConnection();
 //createUserTable($connection);
+//createSaltTable($connection);
+
+//createOfertTable($connection);
+//createDemandTable($connection);
+
+//printTable($connection, "Users");
 //insertUser($connection, "Usuario2","usuario2@usuario.com", "usuariopass");
-printTable($connection, "Users");
+//clearTable($connection, "Users");
+
 
 //Close connection
 mysqli_close($con);
