4190d7921827f9e597f07e1f20ae0fe6258c8df9
heinetron <heisncfr@inf.upv.es>
2013-11-16 16:09:53 +0100

    Adds Offer class to classes.php

--- backend/classes.php
+++ backend/classes.php
@@ -66,4 +66,55 @@ class User{
 	}
 }
 
+class Offer{
+	
+	private $_id;
+	private $_name;
+	private $_category;
+	private $_description;
+	
+		public static function withID($id){
+		$instance = new self();
+		$instance->loadByID($id);
+		return $instance;
+	}
+	
+	private function loadByID($id){
+		$db = new DB();
+		$queryResults = $db->selectTableWithColumn("Offer", "id", $id);
+		if($queryResults){
+			$this->fill($queryResults[0]);
+		} else {
+			return false;
+		}
+	}
+	
+	public static function withName($name){
+		$instance = new self();
+		$instance->loadByName($name);
+		return $instance;
+	}
+	
+	private function loadByName($name){
+		$db = new DB();
+		$queryResults = $db->selectTableWithColumn("Offer", "Name", $name);
+		if($queryResults){
+			$this->fill($queryResults[0]);
+		} else {
+			return false;
+		}
+	}
+
+	public function fill(DBQueryResult $result){
+		$this->_id = $result->id;
+		$this->_name = $result->Name;
+		$this->_category = $result->Category;
+		$this->_description = $result->Description;
+	}	
+	public function __toString(){
+		return "ID: " . $this->_id . 
+		" / Name: " . $this->_name . 
+		" / Category: " . $this->_category;
+	}
+}
 ?>
