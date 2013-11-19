2e5c9a9cffc7fa086299f2ed32fca101bec6a8a4
heinetron <heisncfr@inf.upv.es>
2013-11-19 20:55:58 +0100

    Adds Category class and method getAll() for all classes

--- backend/classes.php
+++ backend/classes.php
@@ -6,7 +6,23 @@ class User{
 	private $_email;
 	private $_password;
 	private $_salt;
+	//todo private $_author;
 		
+	// Loads all user in the database
+	public static function getAll(){
+		$db = new DB();
+		$queryResults = $db->selectAll("User");
+		$users = array();
+		foreach($queryResults as $qr){
+			$user = new User();
+			$user->fill($qr);
+			$users[] = $user;
+		}
+		return $users;
+	}
+	
+	// Loads a user using the ID
+	// $user = User::withID(1)
 	public static function withID($id){
 		$instance = new self();
 		$instance->loadByID($id);
@@ -23,6 +39,8 @@ class User{
 		}
 	}
 	
+	// Loads a user using its name
+	// $user = User::withName("Usuario")
 	public static function withName($name){
 		$instance = new self();
 		$instance->loadByName($name);
@@ -39,6 +57,7 @@ class User{
 		}
 	}
 	
+	// Sets all attributes using a QueryResult array
 	public function fill(DBQueryResult $result){
 		$this->_id = $result->id;
 		$this->_name = $result->Name;
@@ -48,6 +67,8 @@ class User{
 		$this->_premium = $result->Premium;
 	}
 	
+	// Returns true if password matches the user password
+	// if ($user->checkPassword("usuario_pass")
 	public function checkPassword($password){
 		if($this->hashPassword($password, $this->_salt) == $this->_password){
 			return true;
@@ -55,6 +76,7 @@ class User{
 		return false;
 	}
 	
+	// increases password security
 	private function hashPassword($password, $salt){
 		return hash(HASH_ALGO, $salt . $password . $salt);
 	}
@@ -73,7 +95,22 @@ class Offer{
 	private $_category;
 	private $_description;
 	
-		public static function withID($id){
+	// Loads all offers in the database
+	public static function getAll(){
+		$db = new DB();
+		$queryResults = $db->selectAll("Offer");
+		$offers = array();
+		foreach($queryResults as $qr){
+			$offer = new Offer();
+			$offer->fill($qr);
+			$offers[] = $offer;
+		}
+		return $offers;
+	}
+	
+	// Loads an offer using its ID
+	// $offer = Offer::withID(1)
+	public static function withID($id){
 		$instance = new self();
 		$instance->loadByID($id);
 		return $instance;
@@ -89,6 +126,8 @@ class Offer{
 		}
 	}
 	
+	// Loads an offer using its name
+	// $offer = Offer::withName("Oferta1")
 	public static function withName($name){
 		$instance = new self();
 		$instance->loadByName($name);
@@ -105,6 +144,7 @@ class Offer{
 		}
 	}
 
+	// Sets all attributes using a QueryResult array
 	public function fill(DBQueryResult $result){
 		$this->_id = $result->id;
 		$this->_name = $result->Name;
@@ -125,7 +165,22 @@ class Demand{
 	private $_category;
 	private $_description;
 	
-		public static function withID($id){
+	// Loads all demands in the database
+	public static function getAll(){
+		$db = new DB();
+		$queryResults = $db->selectAll("Demand");
+		$demands = array();
+		foreach($queryResults as $qr){
+			$demand = new Demand();
+			$demand->fill($qr);
+			$demands[] = $demand;
+		}
+		return $demands;
+	}
+	
+	// Loads a demand using its ID
+	// $demand = Demand::withID(1)	
+	public static function withID($id){
 		$instance = new self();
 		$instance->loadByID($id);
 		return $instance;
@@ -140,7 +195,8 @@ class Demand{
 			return false;
 		}
 	}
-	
+	// Loads a demand using its name
+	// $demand = Demand::withName("Demanda1")		
 	public static function withName($name){
 		$instance = new self();
 		$instance->loadByName($name);
@@ -157,6 +213,7 @@ class Demand{
 		}
 	}
 
+	// Sets all attributes using a QueryResult array
 	public function fill(DBQueryResult $result){
 		$this->_id = $result->id;
 		$this->_name = $result->Name;
@@ -167,6 +224,71 @@ class Demand{
 		return "ID: " . $this->_id . 
 		" / Name: " . $this->_name . 
 		" / Category: " . $this->_category;
+	}
+}
+
+class Category{
+
+	private $_id;
+	private $_name;
+	
+	// Loads all categories in the database
+	public static function getAll(){
+		$db = new DB();
+		$queryResults = $db->selectAll("Category");
+		$categories = array();
+		foreach($queryResults as $qr){
+			$category = new Dategory();
+			$category->fill($qr);
+			$categories[] = $category;
+		}
+		return $demands;
+	}
+	
+	// Loads a category using its ID
+	// $category = Dategory::withID(1)	
+	public static function withID($id){
+		$instance = new self();
+		$instance->loadByID($id);
+		return $instance;
+	}
+	
+	private function loadByID($id){
+		$db = new DB();
+		$queryResults = $db->selectTableWithColumn("Category", "id", $id);
+		if($queryResults){
+			$this->fill($queryResults[0]);
+		} else {
+			return false;
+		}
+	}
+	// Loads a category using its name
+	// $category = Category::withName("Demanda1")		
+	public static function withName($name){
+		$instance = new self();
+		$instance->loadByName($name);
+		return $instance;
+	}
+	
+	private function loadByName($name){
+		$db = new DB();
+		$queryResults = $db->selectTableWithColumn("Category", "Name", $name);
+		if($queryResults){
+			$this->fill($queryResults[0]);
+		} else {
+			return false;
+		}
+	}
+
+	// Sets all attributes using a QueryResult array
+	public function fill(DBQueryResult $result){
+		$this->_id = $result->id;
+		$this->_name = $result->Name;
+	}
+	
+	public function __toString(){
+		return "ID: " . $this->_id . 
+		" / Name: " . $this->_name;
 	}
 }
 ?>
